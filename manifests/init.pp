33c4e1287dcec86e95e9c497b3c0e31f0ba4f5f0
Huaqing Zheng <huasome@squareup.com>
2016-08-25 14:02:15 -0700

    Disable creating system users.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,7 @@ class rvm(
   $install_dependencies=false,
   $manage_rvmrc=$rvm::params::manage_rvmrc,
   $system_users=[],
+  $system_user_create=false,
   $system_rubies={},
   $rvm_gems={},
   $proxy_url=$rvm::params::proxy_url,
@@ -36,7 +37,7 @@ class rvm(
     }
   }
 
-  rvm::system_user{ $system_users: }
+  rvm::system_user{ $system_users: create => $system_user_create }
   create_resources('rvm_system_ruby', $system_rubies, {'ensure' => present, 'proxy_url' => $proxy_url, 'no_proxy' => $no_proxy})
   if $rvm_gems != {} {
     validate_hash($rvm_gems)
